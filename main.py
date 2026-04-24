217c5dfc294064747016b222e6faa7a29f877267
stack-Tech-1 <stacktechnologies0@gmail.com>
2026-04-24 10:22:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ load_dotenv()
 
 # ── Config ─────────────────────────────────────────────────────────────────────
 BOT_TOKEN    = os.getenv("BOT_TOKEN",          "8747707317:AAG8BGaiU0HRMm-YpRk4hEfIQ0iHsZAhLEc")
-HF_API_TOKEN = os.getenv("HF_API_TOKEN", "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")
+HF_API_TOKEN = os.getenv("HF_API_TOKEN")
 
 # Hugging Face model - free to use
 HF_MODEL = "stabilityai/stable-diffusion-2-1"

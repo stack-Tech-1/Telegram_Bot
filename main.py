56af53c1ea7bb03b0a21709277aac5fb14b27adb
stack-Tech-1 <stacktechnologies0@gmail.com>
2026-04-24 11:06:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ from telegram.ext import (
 load_dotenv()
 
 # ── Config ─────────────────────────────────────────────────────────────────────
-BOT_TOKEN   = os.getenv("BOT_TOKEN", "8747707317:AAG8BGaiU0HRMm-YpRk4hEfIQ0iHsZAhLEc")
+BOT_TOKEN   = os.getenv("BOT_TOKEN","")
 HF_API_TOKEN = os.getenv("HF_API_TOKEN")
 
 if not HF_API_TOKEN:
